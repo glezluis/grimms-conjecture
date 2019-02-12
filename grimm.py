adcc16174d1f6d172afb11052ef582e4aed16f0a
glezluis <luismg@csu.fullerton.edu>
2019-02-11 18:29:27 -0800

    added comments

--- grimm.py
+++ grimm.py
@@ -1,21 +1,25 @@
 from sympy import isprime, primefactors
 
-def set_primes(s):
+def set_p#rimes(s):
     for composite in sorted(s, reverse = True):
         for prime in reversed(primefactors(composite)):
             if prime not in s.values():
                 s[composite] = prime
                 break
         if s[composite] == 0:
-            raise Exception('no unique prime found')
+            raise Exception('no unique prime found for %d' % composite)
     return s
 
+# Algorithm that finds all the sets of consecutive composites between the give range
+# input: a tuple r of two postive integers
+# output: a dictionary whos key is the number of the set and value is another dictionary.
+#		  That dictionary's key is the composite number and value is the unique prime
 def consecutive_composites(r):
     a, b = r
     clist = [i for i in range(a,b) if not isprime(i)]
-    ccsets = dict()
+    ccsets = {}
+    singleset = {}
     setkey = 1
-    singleset = dict()
 
     for i in range(len(clist)-1):
         n1 = clist[i]
@@ -23,25 +27,36 @@ def consecutive_composites(r):
         if n2 - n1 == 1:
             if singleset and n1 - list(singleset.keys())[-1] > 1:
                 ccsets[setkey] = set_primes(singleset)
-                setkey = setkey + 1
+                setkey += 1
                 singleset = {}
             singleset[n1] = 0
             singleset[n2] = 0
-    ccsets[setkey] = set_primes(singleset)
+ 
+    if singleset:
+    	ccsets[setkey] = set_primes(singleset)
+
     return ccsets            
 
+# Prints the set of of consecutive composites
+# input: none
+# output: none
 def print_comps(s): 
     for index, x  in s.items():
         print(index, ". ", x, "\n")
-    
+
+# Prints the menu
+# input: none
+# output: none
 def menu():
     print('\nGrimm\'s conjecture! what do you want to? ')
     print('(f)ind consectuive composite sets')
     print('(p)rint sets')
     print('(q)uit')
 
+#This function handles all the input for the program
+# input: none
+# output: tuple of two positive integers
 def input_range():
-    
     print('Enter a range [a,b]')
 
     while True: 
@@ -49,10 +64,10 @@ def input_range():
             a = int(input('a = '))
             b = int(input('b = '))
             if a < 0 or b < 0:
-                print('only enter positive values!')
+                print('*** only enter positive values! ***')
                 continue
             return((a,b))
         except:
-            print("enter a valid integer!")
+            print("*** enter a valid integer! ***")
 
    
--- main.py
+++ main.py
@@ -3,23 +3,27 @@ from grimm import *
 def main():
     menuval = ''
 
-    while menuval is not 'q':
+    while menuval != 'q':
         menu()
-        menuval = input('enter a choice: ')
-        if menuval is 'f':
+        menuval = input('enter a choice: ').lower()
+
+        if menuval == 'f':
             a,b = input_range()
             allsets = consecutive_composites((a,b))
-            if len(allsets) == 0:
+
+            if allsets:
                 print('%d consecutive composite sets found between [%d, %d]' % (len(allsets),  a, b))
             else:
                 print('no consecutive composite sets found')
-        elif menuval is 'p':
+
+        elif menuval == 'p':
             if not allsets: 
                 print_comps(consecutive_composites(input_range()))
             else:
                 print_comps(allsets)
-        elif menuval is not 'q':
-            print('enter valid input')
+
+        elif menuval != 'q':
+            print('\n*** enter valid input ***')
 
 if __name__ == '__main__':
     main()
